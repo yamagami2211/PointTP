ff6c2a8de441bd37184bb8cc6b20da032ccb94d5
yamagami2211 <yamagami2211@svk.jp>
2019-08-25 15:56:48 +0900

    間違ってた部分を直して、改行とかが崩れてたのを直した(つもり)

--- src/main/java/net/yama2211/ps/Main.java
+++ src/main/java/net/yama2211/ps/Main.java
@@ -2,11 +2,7 @@
 
   @author : yamagami2211 < https://yama2211.biz | https://twitter.com/yamagami2211_02 >
 
-  ver: 1.0.0.21 | ブロックの角に飛ぶのを直したい....
-  ver: 1.0.0.20 | ブロックの角に飛ぶのを直したい....
-  ver: 1.0.0.19 | ブロックの角に飛ぶのを直したい....
-  ver: 1.0.0.18 | ブロックの角に飛ぶのを直したい....
-  ver: 1.0.0.17 | テレポートコマンド(/ps tp)が使えねぇ...
+  ver: 1.0.0.23 | 大体完成してるけどAlpha版だよぉ...
 
 //*/
 package net.yama2211.ps;
@@ -35,102 +31,96 @@ public class Main extends JavaPlugin implements Listener {
 	}
 
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
-	    if(cmd.getName().equalsIgnoreCase("pointtp"))
-	    {
- 	       if(args.length == 0)
- 	       {
- 	    	  PluginDescriptionFile yml = getDescription();
-				sender.sendMessage(ChatColor.LIGHT_PURPLE + "===buildblock===");
+		if (cmd.getName().equalsIgnoreCase("pointtp")) {
+			if (args.length == 0) {
+				PluginDescriptionFile yml = getDescription();
+				sender.sendMessage(ChatColor.LIGHT_PURPLE + "===PointTP===");
 				sender.sendMessage(ChatColor.GREEN + "PluginVersion : " + ChatColor.DARK_PURPLE + yml.getVersion());
 				sender.sendMessage(ChatColor.AQUA + "/pointtp setpoint" + ChatColor.WHITE + ": ポイントを設定します。");
 				sender.sendMessage(ChatColor.AQUA + "/pointtp tp" + ChatColor.WHITE + ": ポイントにテレポートします。");
- 	       }
- 	       if(args.length == 1)
- 	       {
- 	    	  if(args[0].equalsIgnoreCase("setpoint"))
- 	    	  {
- 	    		 if ((sender.hasPermission("pointtp.setpoint")) || (sender.isOp())) {
- 	    		 if (sender instanceof Player) {
+			}
+			if (args.length == 1) {
+				if (args[0].equalsIgnoreCase("setpoint")) {
+					if ((sender.hasPermission("pointtp.setpoint")) || (sender.isOp())) {
+						if (sender instanceof Player) {
 
- 			Player player = (Player)sender;
+							Player player = (Player) sender;
 
+							getConfig().set("point" + ".World", player.getLocation().getWorld().getName());
+							getConfig().set("point" + ".X", player.getLocation().getBlockX());
+							getConfig().set("point" + ".Y", player.getLocation().getBlockY());
+							getConfig().set("point" + ".Z", player.getLocation().getBlockZ());
+							getConfig().set("point" + ".Yaw", player.getLocation().getYaw());
+							getConfig().set("point" + ".Pitch", player.getLocation().getPitch());
+							saveConfig();
+							sender.sendMessage(ChatColor.AQUA + "[PointTP] " + ChatColor.GREEN + "座標をconfigに記録しました");
+						} else {
+							sender.sendMessage(
+									"このコマンドは" + ChatColor.RED + "コンソール" + ChatColor.RESET + "から実行することはできません。");
+						}
 
- 	          getConfig().set(args[0] + ".World", player.getLocation().getWorld().getName() );
- 	          getConfig().set("point" + ".X", player.getLocation().getBlockX() );
- 	          getConfig().set("point" + ".Y", player.getLocation().getBlockY() );
- 	          getConfig().set("point" + ".Z", player.getLocation().getBlockZ() );
- 	          getConfig().set("point" + ".Yaw",player.getLocation().getYaw() );
- 	          getConfig().set("point" + ".Pitch", player.getLocation().getPitch() );
- 			saveConfig();
-			sender.sendMessage(ChatColor.AQUA + "[PointTP] " + ChatColor.GREEN + "座標をconfigに記録しました");
- 			} else {
-    			 sender.sendMessage("このコマンドは" + ChatColor.RED + "コンソール" + ChatColor.RESET + "から実行することはできません。");
-    			 }
+					}
+				}
 
- 	    		 }
- 	    	  }
+				if (args[0].equalsIgnoreCase("tp")) {
+					if ((sender.hasPermission("pointtp.teleport")) || (sender.isOp())) {
+						if (sender instanceof Player) {
 
- 	    	  if(args[0].equalsIgnoreCase("tp"))
- 	    	  {
- 	    		 if ((sender.hasPermission("pointtp.teleport")) || (sender.isOp())) {
- 	    		 if (sender instanceof Player) {
+							Player player = (Player) sender;
+							//boolean age = args.length == 2;
 
- 			Player player = (Player)sender;
- 			//boolean age = args.length == 2;
+							float yaw = (float) getConfig().getDouble("point" + ".Yaw");
+							float pitch = (float) getConfig().getDouble("point" + ".Pitch");
+							double x = getConfig().getDouble("point" + ".X") + 0.5;
+							double y = getConfig().getDouble("point" + ".Y");
+							double z = getConfig().getDouble("point" + ".Z") + 0.5;
+							World w = Bukkit.getServer().getWorld(getConfig().getString("point" + ".World"));
 
-            float yaw = (float) getConfig().getDouble("point"+".Yaw");
-            float pitch = (float) getConfig().getDouble("point"+".Pitch");
-            double x = getConfig().getDouble("point"+".X") + 0.5;
-            double y = getConfig().getDouble("point"+".Y");
-            double z = getConfig().getDouble("point"+".Z") + 0.5;
- 			World w = Bukkit.getServer().getWorld(getConfig().getString("point"+".World"));
+							Location point = new Location(w, x, y, z);
+							point.setYaw(yaw);
+							player.getLocation().setPitch(pitch);
 
- 			Location point = new Location (w, x, y, z);
- 			point.setYaw(yaw);
- 			player.getLocation().setPitch(pitch);
+							player.teleport(point);
+							sender.sendMessage(ChatColor.AQUA + "[PointTP] " + ChatColor.GREEN + "テレポートしました。");
+						} else {
+							sender.sendMessage(
+									"このコマンドは" + ChatColor.RED + "コンソール" + ChatColor.RESET + "から実行することはできません。");
+						}
+					}
+				}
 
- 			player.teleport(point);
- 			sender.sendMessage(ChatColor.AQUA + "[PointTP] " + ChatColor.GREEN + "テレポートしました。");
- 	    		 } else {
- 	    			 sender.sendMessage("このコマンドは" + ChatColor.RED + "コンソール" + ChatColor.RESET + "から実行することはできません。");
- 	    			 }
- 	    		 }
- 	    	  }
+				if (args[0].equalsIgnoreCase("reload")) {
+					if ((sender.hasPermission("pointtp.cofreload")) || (sender.isOp())) {
+						reloadConfig();
+						sender.sendMessage(ChatColor.AQUA + "[PointTP] " + ChatColor.GREEN + "Configをリロードしました。");
+					}
+				}
 
- 	    	  if(args[0].equalsIgnoreCase("reload"))
- 	    	  {
- 	    		 if ((sender.hasPermission("pointtp.cofreload")) || (sender.isOp())) {
- 	    		 reloadConfig();
- 	    		 sender.sendMessage(ChatColor.AQUA + "[PointTP] " + ChatColor.GREEN + "Configをリロードしました。");
- 	    	  }
- 	    	  }
+			}
 
-	    }
+		}
 
-	}
-
-return true;
+		return true;
 	}
 
 	@EventHandler
 	public void onJoinEvent(PlayerJoinEvent e) {
-		if(getConfig().getBoolean("JoinTP") == true) {
+		if (getConfig().getBoolean("JoinTP") == true) {
 
 			Player player = e.getPlayer();
-            float yaw = (float) getConfig().getDouble("point"+".Yaw");
-            float pitch = (float) getConfig().getDouble("point"+".Pitch");
-            double x = getConfig().getDouble("point"+".X") + 0.5;
-            double y = getConfig().getDouble("point"+".Y");
-            double z = getConfig().getDouble("point"+".Z") + 0.5;
- 			World w = Bukkit.getServer().getWorld(getConfig().getString("point"+".World"));
+			float yaw = (float) getConfig().getDouble("point" + ".Yaw");
+			float pitch = (float) getConfig().getDouble("point" + ".Pitch");
+			double x = getConfig().getDouble("point" + ".X") + 0.5;
+			double y = getConfig().getDouble("point" + ".Y");
+			double z = getConfig().getDouble("point" + ".Z") + 0.5;
+			World w = Bukkit.getServer().getWorld(getConfig().getString("point" + ".World"));
 
- 			Location point = new Location (w, x, y, z);
- 			point.setYaw(yaw);
- 			player.getLocation().setPitch(pitch);
+			Location point = new Location(w, x, y, z);
+			point.setYaw(yaw);
+			player.getLocation().setPitch(pitch);
 
- 			player.teleport(point);
+			player.teleport(point);
 		}
 	}
-
 
 }
